3c10aadf1729b657c5fc8feccf8f7f42a067d615
Andrei Yasinski <andrewtopol856@gmail.com>
2019-04-05 11:13:20 +0300

    update task 6

--- homeWork_3/task_6.js
+++ homeWork_3/task_6.js
@@ -1,7 +1,17 @@
 function getMultiplicationTable(num) {
   var result = '';
   for (let i = 1; i <= 10; i++) {
+    console.log(num + ' * ' + i + ' = ' + num*i)
+  }
+}
+
+
+/*
+function getMultiplicationTable(num) {
+  var result = '';
+  for (let i = 1; i <= 10; i++) {
     result += num + ' * ' + i + ' = ' + num*i + '\r\n'
   }
   return console.log(result);
 }
+*/
